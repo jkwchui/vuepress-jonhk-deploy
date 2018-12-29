96603d761d96510879d84956549a1ec8e6a188cf
Jon Chui <jon@lpcuwc.edu.hk>
2018-12-29 23:33:07 +0800

    edited config.js
    Signed-off-by: Jon Chui <jon@lpcuwc.edu.hk>

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -8,16 +8,47 @@ module.exports = {
         }
     },
     head: [
-        ['link', { rel: 'icon', href: `/logo.png` }],
-        ['link', { rel: 'manifest', href: '/manifest.json' }],
-        ['link', { rel: 'canonical', href: 'https://vuepress-deploy.netlify.com' }],
-        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-        ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
-        ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-        ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-        ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+        ['link', {
+            rel: 'icon',
+            href: `/logo.png`
+        }],
+        ['link', {
+            rel: 'manifest',
+            href: '/manifest.json'
+        }],
+        ['link', {
+            rel: 'canonical',
+            href: 'https://vuepress-deploy.netlify.com'
+        }],
+        ['meta', {
+            name: 'theme-color',
+            content: '#3eaf7c'
+        }],
+        ['meta', {
+            name: 'apple-mobile-web-app-capable',
+            content: 'yes'
+        }],
+        ['meta', {
+            name: 'apple-mobile-web-app-status-bar-style',
+            content: 'black'
+        }],
+        ['link', {
+            rel: 'apple-touch-icon',
+            href: `/icons/apple-touch-icon-152x152.png`
+        }],
+        ['link', {
+            rel: 'mask-icon',
+            href: '/icons/safari-pinned-tab.svg',
+            color: '#3eaf7c'
+        }],
+        ['meta', {
+            name: 'msapplication-TileImage',
+            content: '/icons/msapplication-icon-144x144.png'
+        }],
+        ['meta', {
+            name: 'msapplication-TileColor',
+            content: '#000000'
+        }]
     ],
     serviceWorker: true,
     themeConfig: {
@@ -26,7 +57,7 @@ module.exports = {
 
         nav: [{
                 text: 'Start',
-                link: '/docs/',
+                link: '/pages/',
             },
             {
                 text: 'Admin',
--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 home: true
 pageClass: front
 actionText: Hello VuePress →
-actionLink: /pages/
+actionLink: /docs/
 features:
   - title: One-click Deploy
     details: Use the ”Deploy to Netlify“ button below to create a new VuePress installation with one simple click. Get my wonderful VuePress theme for free.
   - title: Github
     details: You need a Github and a Netlify account. Otherwise the Deploy Button doesn't work.
   - title: Netlify
-    details: The “Deploy to Netlify” button helps users deploy new sites from templates with one single click on Netlify.
+    details: The “Deploy to Netlify” button helps users deploy new sites from templates with one single click on Netlify.  It's handy.
 ---
 
 # VuePress Deploy
